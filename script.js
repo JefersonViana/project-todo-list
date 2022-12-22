cabf55a60753107a1c5288d53c7d60373c95a1a6
jefersonviana <jefersonv28@gmail.com>
2022-12-22 16:17:04 -0300

    Requisito 1 concluído

---
+++ script.js
@@ -0,0 +1,5 @@
+const body = document.querySelector('body');
+const header = document.createElement('header');
+header.id = 'title';
+header.innerText = 'Minha Lista de Tarefas';
+body.appendChild(header);
