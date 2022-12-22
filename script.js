8e68cd9d2eacac48f77051ee4bcf1e6572a7f159
jefersonviana <jefersonv28@gmail.com>
2022-12-22 16:38:33 -0300

    Requisitos 4 e 5 concluídos

--- script.js
+++ script.js
@@ -7,10 +7,23 @@ paragraph.id = 'funcionamento';
 paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 const input = document.createElement('input');
 input.id = 'texto-tarefa';
+const button = document.createElement('button');
+button.id = 'criar-tarefa';
 const listaOrdenada = document.createElement('ol');
 listaOrdenada.id = 'lista-tarefas';
 
 body.appendChild(header);
 body.appendChild(paragraph);
 body.appendChild(input);
+body.appendChild(button);
 body.appendChild(listaOrdenada);
+
+const listarTarefa = () => {
+  const tarefa = input.value;
+  const criarItem = document.createElement('li');
+  criarItem.innerText = tarefa;
+  listaOrdenada.appendChild(criarItem);
+  input.value = '';
+};
+
+button.addEventListener('click', listarTarefa);
