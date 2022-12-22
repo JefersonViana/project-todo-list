53f35cc78cc2e011f748cdfa3ed4d120857399d2
jefersonviana <jefersonv28@gmail.com>
2022-12-22 16:26:56 -0300

    Requisito 4 concluído

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ paragraph.id = 'funcionamento';
 paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 const input = document.createElement('input');
 input.id = 'texto-tarefa';
+const listaOrdenada = document.createElement('ol');
+listaOrdenada.id = 'lista-tarefas';
 
 body.appendChild(header);
 body.appendChild(paragraph);
 body.appendChild(input);
+body.appendChild(listaOrdenada);
