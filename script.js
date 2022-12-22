1fdb73eee86e5412c6c6a0dc735a33df6a0e0858
jefersonviana <jefersonv28@gmail.com>
2022-12-22 16:19:50 -0300

    Requisito 2 concluído

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ const body = document.querySelector('body');
 const header = document.createElement('header');
 header.id = 'title';
 header.innerText = 'Minha Lista de Tarefas';
+const paragraph = document.createElement('p');
+paragraph.id = 'funcionamento';
+paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 body.appendChild(header);
+body.appendChild(paragraph);
